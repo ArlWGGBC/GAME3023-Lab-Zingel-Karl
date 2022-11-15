70d262707f6f3fd6a46a6286d7d3ba107eee6276
Karl Zingel <82173548+ArlWGGBC@users.noreply.github.com>
2022-11-15 14:28:16 -0500

    Added animation (lab exercise 5)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -12,12 +14,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     Rigidbody2D rigidbody;
 
+    [SerializeField]
+    private Animator _animator;
+
+    private static readonly int XSpeed = Animator.StringToHash("x_speed");
+    private static readonly int YSpeed = Animator.StringToHash("y_speed");
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World");
         
         rigidbody = GetComponent<Rigidbody2D>();
+
+        if (_animator == null)
+            _animator = FindObjectOfType<Animator>();
     }
 
     // Update is called once per frame
@@ -26,10 +38,45 @@ public class PlayerController : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
+        Debug.Log(x);
         Vector3 oldPosition = transform.position;
 
         rigidbody.MovePosition(oldPosition + new Vector3(x, y, 0) * (Speed * Time.deltaTime));
-        // transform.position = oldPosition + new Vector3(xInput, yInput, 0) * MoveSpeed * Time.deltaTime;
+        
+        SetAnimationValues(x, y);
+       
+    }
+
+
+    private void SetAnimationValues(float x, float y)
+    {
+        
+        
+        _animator.SetFloat(XSpeed, x);
+        _animator.SetFloat(YSpeed, y);
+
+        if (isEqual(x, y))
+        {
+            _animator.SetBool(IsMoving, false);
+        }
+        else
+        {
+            _animator.SetBool(IsMoving, true);
+        }
+
+
+       // gameObject.transform.localRotation = Quaternion.Euler(0.0f, x > 0 ? 0.0f : 180.0f, 0f);
+        
+        
+        
+      
+    }
+
+    private bool isEqual(float x, float y)
+    {
+        float margin = 0.1f;
+
+        return math.abs(x - y) <= margin * math.abs(x);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
